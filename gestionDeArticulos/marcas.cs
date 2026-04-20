1a8a331871de7de082493c7d929d9e0c495c9471
snleg <snleg@NAHUEL>
2026-04-20 17:56:51 -0300

    Agregar archivos de proyecto.

---
+++ gestionDeArticulos/marcas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestionDeArticulos
+{
+    public class marcas
+    {
+        public int IdMarcas { get; set; }
+
+        public string descripcionMarca { get; set; }
+
+    }
+}
