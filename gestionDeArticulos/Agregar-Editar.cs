520ff3633ede962810dac501804025b5e73861f5
Nahuel Leguizamon <snleguizamon@gmail.com>
2026-04-22 16:58:18 -0300

    AGREGAMOS FORMULARIO AGREGAR Y RELLENAMOS LOS COMBOBOX DE MARCA Y CATEGORIA

---
+++ gestionDeArticulos/Agregar-Editar.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gestionDeArticulos
+{
+    public partial class Agregar_Editar : Form
+    {
+        public Agregar_Editar()
+        {
+            InitializeComponent();
+            DataTable dtMarca = rellenarCbMarca();
+            DataTable dtCategoria= rellenarCbCategoria();
+            if (dtMarca.Rows.Count > 0) {
+
+                cbMarca.DataSource = dtMarca;
+                cbMarca.ValueMember = "Id";
+                cbMarca.DisplayMember = "Descripcion";
+                
+            }
+            if (dtCategoria.Rows.Count > 0) {
+            
+                cbCategoria.DataSource = dtCategoria;
+                cbCategoria.ValueMember="Id";
+                cbCategoria.DisplayMember = "Descripcion";
+            }
+        }
+
+       public DataTable rellenarCbCategoria()
+        {
+
+            string ruta = "Server=localhost\\SQLEXPRESS;database=CATALOGO_P3_DB;integrated security=true";
+            string consulta = "select id,Descripcion from CATEGORIAS";
+            DataTable dt = new DataTable();
+            using (SqlConnection con = new SqlConnection(ruta)) 
+            {
+                try
+                {
+                    SqlDataAdapter Da = new SqlDataAdapter(consulta,ruta);
+                    Da.Fill(dt);
+                }
+                catch (Exception ex)
+                {
+
+                    throw ex;
+                }
+
+
+            }
+            return dt;
+        }
+
+        public DataTable rellenarCbMarca()
+        {
+
+            string ruta = "Server=localhost\\SQLEXPRESS;database=CATALOGO_P3_DB;integrated security=true";
+            string consulta = "select id,Descripcion from MARCAS";
+            DataTable dt = new DataTable();
+            using (SqlConnection con = new SqlConnection(ruta))
+            {
+                try
+                {
+                    SqlDataAdapter Da = new SqlDataAdapter(consulta, ruta);
+                    Da.Fill(dt);
+                }
+                catch (Exception ex)
+                {
+
+                    throw ex;
+                }
+
+
+            }
+            return dt;
+        }
+
+
+
+
+    }
+}
--- gestionDeArticulos/Form1.Designer.cs
+++ gestionDeArticulos/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.btnAtras = new System.Windows.Forms.Button();
             this.btnAdelante = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pcbImagen)).BeginInit();
             this.SuspendLayout();
@@ -86,11 +87,22 @@
             this.btnAdelante.UseVisualStyleBackColor = true;
             this.btnAdelante.Click += new System.EventHandler(this.btnAdelante_Click);
             // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(624, 482);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1105, 609);
+            this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.btnAdelante);
             this.Controls.Add(this.btnAtras);
             this.Controls.Add(this.label2);
@@ -113,6 +125,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnAtras;
         private System.Windows.Forms.Button btnAdelante;
+        private System.Windows.Forms.Button btnAgregar;
     }
 }
 
